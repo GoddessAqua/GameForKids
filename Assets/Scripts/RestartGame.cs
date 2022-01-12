85eafebd3d79056268ef880fd2bcf2af6a674407
RedunViktoria <45935753+RedunViktoria@users.noreply.github.com>
2022-01-12 20:54:57 +0300

    Миссия выполнена

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour,  IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
