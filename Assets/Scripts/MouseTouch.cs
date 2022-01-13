f8081b5f4d008c809577a8426553a09059548a92
RedunViktoria <45935753+RedunViktoria@users.noreply.github.com>
2022-01-13 13:08:29 +0300

    Добавление системы частиц
    Добавлена система частиц к элементам

--- Assets/Scripts/MouseTouch.cs
+++ Assets/Scripts/MouseTouch.cs
@@ -22,20 +22,31 @@ public class MouseTouch: MonoBehaviour,  IPointerDownHandler
         }
     } 
     private GameObject _choosenElement;
+
+    [SerializeField] private ParticleSystem _stars;
+    
+    void Awake()
+    {
+        _stars.Stop();
+    }
+    
     void Start()
     {
+        _stars.Stop();
         _dataFromCardCell = FindObjectOfType<CardGrid>();
         Cam = FindObjectOfType<Camera>();
     }
     
     public void OnPointerDown(PointerEventData eventData)
     {
+        _stars.Stop();
         DOTween.CompleteAll();
         if (eventData.button == PointerEventData.InputButton.Left)
         {
             RaycastHit2D aHit = new RaycastHit2D();
             aHit = Physics2D.Raycast(Cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             _choosenElement = aHit.collider.gameObject;
+            
             if (_dataFromCardCell.GetTask == _choosenElement.GetComponent<MouseTouch>()._id)
             {
                 _choosenElement.transform.DOScale(new Vector3(1.2f,1.2f,1.2f), 2f).SetLoops(2, LoopType.Yoyo);
@@ -43,7 +54,8 @@ public class MouseTouch: MonoBehaviour,  IPointerDownHandler
             }
             else
             {
-                StartCoroutine(SomeCoroutine());        
+                _stars.Play();
+                StartCoroutine(WaitAnimation());    
             }
         }
     }
@@ -64,9 +76,10 @@ public class MouseTouch: MonoBehaviour,  IPointerDownHandler
         GenerateNewLevel();
     }
 
-    IEnumerator SomeCoroutine()
+    IEnumerator WaitAnimation()
     {
         Tween myTween = _choosenElement.transform.DOShakePosition(1f, strength: new Vector3(0,1,0), vibrato: 4, randomness: 1, snapping: false, fadeOut: true).SetLoops(1,LoopType.Yoyo);
         yield return myTween.WaitForCompletion();
+        _stars.Stop();  
     }
 }
